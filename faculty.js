88e45f41aa34a8da3b80009152f7fe5d030c22da
shuseng0609-collab <shuseng0609@gmail.com>
2025-12-24 22:35:21 +0800

    Update faculty.js

--- faculty.js
+++ faculty.js
@@ -1,1 +1,4 @@
-document.getElementById("teacher-list").innerHTML = "<li>JS 有執行 ✅</li>";
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("teacher-list").innerHTML =
+    "<li>JS 有執行 ✅（DOM Ready）</li>";
+});
